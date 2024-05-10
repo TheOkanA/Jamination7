19ff28b5261be60e46c781d7afa46f36695f1977
Bzoth <149201688+Bzoth@users.noreply.github.com>
2024-05-10 23:13:28 +0300

    Commit 6
    2D bug

--- Assets/Scripts/Player/PlayerWalk.cs
+++ Assets/Scripts/Player/PlayerWalk.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Walk : MonoBehaviour
+public class PlayerWalk : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
